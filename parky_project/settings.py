1c0987970ca8e646230bc1056d540f2f56fcd7e3
ravid barzily <ravidbarzily24@gmail.com>
2026-03-12 23:52:14 +0200

    Secure settings with env variables and add car API validation

--- parky_project/settings.py
+++ parky_project/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-me-in-production-abc123xyz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
